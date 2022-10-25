2862fedcfcb1d832b7e4d6c499507100c627a780
Oliver King <kingoliver@microsoft.com>
2022-10-25 16:26:12 -0400

    trying with js

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
    preset: 'ts-jest',
    testEnvironment: 'node',
-   roots: [path.join(__dirname, 'src/test/e2e')],
-   testMatch: [path.join(__dirname, 'src/test/e2e/**/*.test.ts')],
+   roots: [path.join(__dirname, 'out/test/e2e')],
+   testMatch: [path.join(__dirname, 'out/test/e2e/**/*.test.js')],
    collectCoverage: true,
    verbose: true,
    runInBand: true,
