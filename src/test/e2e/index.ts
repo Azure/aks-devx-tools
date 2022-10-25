faf55be46725cce64186cb5d3768da3a1057db6e
Oliver King <kingoliver@microsoft.com>
2022-10-25 17:17:56 -0400

    reverted to old style

--- src/test/e2e/index.ts
+++ src/test/e2e/index.ts
@@ -5,38 +5,31 @@ import * as path from 'path';
 const extensionRoot = path.join(__dirname, '..', '..', '..');
 type Output = {results: AggregatedResult};
 
-export function run(): Promise<void> {
+export function run(
+   testsRoot: string,
+   reportTestResults: (error?: Error, failures?: number) => void
+): void {
    // hack from https://github.com/DonJayamanne/gitHistoryVSCode/blob/75575aa03d03a06a2b901944d65a0f58f4875e02/test/extension/testRunner.ts#L15
-   // Jest doesn't let you inject global imports
+   // because Jest doesn't let you inject global imports
    (process as any).__VSCODE = require('vscode');
 
    // load Jest config
    const jestConfigFile = path.join(extensionRoot, 'jest.e2e.config.js');
    const jestConfig = require(jestConfigFile);
 
-   return new Promise((resolve, reject) => {
-      // run Jest
-      jest
-         .runCLI(jestConfig, [extensionRoot])
-         .catch((err) => {
-            console.error('Calling jest.runCLI failed', err);
-            reject(err);
-         })
-         .then((out) => {
-            if (!out) {
-               return resolve();
-            }
-
-            // if there's any failed tests we should fail
-            const {results} = out as Output;
-            if (results.numFailedTestSuites || results.numFailedTests) {
-               return reject();
-            }
-
-            resolve();
-         })
-         .finally(() => {
-            delete (process as any).__VSCODE;
-         });
-   });
+   jest
+      .runCLI(jestConfig, [extensionRoot])
+      .catch((err) => {
+         reportTestResults(err, 1);
+      })
+      .then((out) => {
+         const {results} = out as Output;
+         reportTestResults(
+            undefined,
+            results.numFailedTestSuites + results.numFailedTests
+         );
+      })
+      .finally(() => {
+         delete (process as any).__VSCODE;
+      });
 }
