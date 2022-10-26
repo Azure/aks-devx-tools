5274d4d72bb64ee02710cd76be5c17f8a01e1913
Oliver King <kingoliver@microsoft.com>
2022-10-26 11:13:24 -0400

    throw error pattern

--- src/test/e2e/index.ts
+++ src/test/e2e/index.ts
@@ -5,10 +5,7 @@ import * as path from 'path';
 const extensionRoot = path.join(__dirname, '..', '..', '..');
 type Output = {results: AggregatedResult};
 
-export function run(
-   testsRoot: string,
-   reportTestResults: (error?: Error, failures?: number) => void
-): void {
+export async function run(): Promise<void> {
    // hack from https://github.com/DonJayamanne/gitHistoryVSCode/blob/75575aa03d03a06a2b901944d65a0f58f4875e02/test/extension/testRunner.ts#L15
    // because Jest doesn't let you inject global imports
    (process as any).__VSCODE = require('vscode');
@@ -17,19 +14,22 @@ export function run(
    const jestConfigFile = path.join(extensionRoot, 'jest.e2e.config.js');
    const jestConfig = require(jestConfigFile);
 
-   jest
-      .runCLI(jestConfig, [extensionRoot])
-      .catch((err) => {
-         reportTestResults(err, 1);
-      })
-      .then((out) => {
-         const {results} = out as Output;
-         reportTestResults(
-            undefined,
-            results.numFailedTestSuites + results.numFailedTests
-         );
-      })
-      .finally(() => {
-         delete (process as any).__VSCODE;
-      });
+   return new Promise<void>((resolve, reject) => {
+      jest
+         .runCLI(jestConfig, [extensionRoot])
+         .catch((err) => {
+            delete (process as any).__VSCODE;
+            return reject(err);
+         })
+         .then((out) => {
+            delete (process as any).__VSCODE;
+            const {results} = out as Output;
+
+            if (results.numFailedTestSuites || results.numFailedTests) {
+               return reject('failed tests');
+            }
+
+            return resolve();
+         });
+   });
 }
