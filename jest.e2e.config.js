c163f6a6bcc54d2e2aa8180fca320306e1001e70
Oliver King <kingoliver@microsoft.com>
2022-10-26 15:19:14 -0400

    trying settings

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -5,11 +5,6 @@ module.exports = {
    testEnvironment: 'node',
    roots: [path.join(__dirname, 'src/test/e2e')],
    testMatch: [path.join(__dirname, 'src/test/e2e/**/*.test.ts')],
-   collectCoverage: true,
    verbose: true,
-   runInBand: true,
-   cache: false,
-   useStderr: true,
-   forceExit: true,
-   detectOpenHandles: true
+   cache: false
 };
