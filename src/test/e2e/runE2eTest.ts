1a258b989cb0d813a1b9ea2726667f0313bb1373
Oliver King <kingoliver@microsoft.com>
2022-10-26 09:59:20 -0400

    move runner

---
+++ src/test/e2e/runE2eTest.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import {runTests} from '@vscode/test-electron';
+
+async function main() {
+   try {
+      const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+      const extensionTestsPath = path.resolve(__dirname, './index');
+
+      // Download VS Code, unzip it and run the integration test
+      await runTests({extensionDevelopmentPath, extensionTestsPath});
+   } catch (err) {
+      console.error(err);
+      process.exit(1);
+   }
+}
+
+main();
