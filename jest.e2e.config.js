d255f3a9758bde2fd52fa516d6163942e79e6da6
Oliver King <kingoliver@microsoft.com>
2022-10-26 10:52:20 -0400

    switch back to ts

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+   preset: 'ts-jest',
    testEnvironment: 'node',
-   roots: [path.join(__dirname, 'out/test/e2e')],
-   testMatch: [path.join(__dirname, 'out/test/e2e/**/*.test.js')],
+   roots: [path.join(__dirname, 'src/test/e2e')],
+   testMatch: [path.join(__dirname, 'src/test/e2e/**/*.test.ts')],
    collectCoverage: true,
    verbose: true,
    runInBand: true,
