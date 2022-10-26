f910ba3d00a1e4fbb8318fec8240c6fd8fb4bcfd
Oliver King <kingoliver@microsoft.com>
2022-10-26 10:12:39 -0400

    add verbose logging

--- src/test/e2e/runE2eTest.ts
+++ src/test/e2e/runE2eTest.ts
@@ -9,7 +9,12 @@ async function main() {
       // Download VS Code, unzip it and run the integration test
       const platform =
          process.platform === 'win32' ? 'win32-x64-archive' : undefined;
-      await runTests({extensionDevelopmentPath, extensionTestsPath, platform});
+      await runTests({
+         extensionDevelopmentPath,
+         extensionTestsPath,
+         platform,
+         launchArgs: ['--verbose']
+      });
    } catch (err) {
       console.error(err);
       process.exit(1);
