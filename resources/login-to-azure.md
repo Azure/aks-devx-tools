b27038829b8622f598b3bfce1c7621059a16f6c3
Oliver King <olivermerkleyking@gmail.com>
2022-11-22 14:06:35 -0500

    Small improvements to walkthrough and defaults (#71)
    * add sort
    
    * remove bad chars in application name guess
    
    * update walkthrough

--- resources/login-to-azure.md
+++
@@ -1,3 +0,0 @@
-# Log in to Azure Account Extension
-
-Ensure that you are logged into the Azure Account extension. Follow the [docs](https://marketplace.visualstudio.com/items?itemName=ms-vscode.azure-account).
--- src/commands/runDraftTool/runDraftDeployment.ts
+++ src/commands/runDraftTool/runDraftDeployment.ts
@@ -42,6 +42,7 @@ import {
 } from './helper/draftCommandBuilder';
 import {image} from '../../utils/acr';
 import {CompletedSteps} from './model/guidedExperience';
+import {sort} from '../../utils/sort';
 
 const title = 'Draft a Kubernetes Deployment and Service';
 
@@ -105,7 +106,9 @@ export async function runDraftDeployment(
    }
 
    const applicationNameGuess =
-      workspaceFolders === undefined ? '' : workspaceFolders[0].name;
+      workspaceFolders === undefined
+         ? ''
+         : workspaceFolders[0].name.toLowerCase().replace(/[^a-z0-9-]+/g, '');
    const wizardContext: WizardContext = {
       ...actionContext,
       port: state.getPort(),
@@ -240,11 +243,14 @@ class PromptNamespace extends AzureWizardPromptStep<WizardContext> {
             }
          ];
       };
-      const namespacePick = await wizardContext.ui.showQuickPick(getOptions(), {
-         ignoreFocusOut,
-         stepName: 'Namespace',
-         placeHolder: 'Namespace'
-      });
+      const namespacePick = await wizardContext.ui.showQuickPick(
+         sort(getOptions()),
+         {
+            ignoreFocusOut,
+            stepName: 'Namespace',
+            placeHolder: 'Namespace'
+         }
+      );
 
       if (namespacePick.label === newOption) {
          wizardContext.newNamespace = true;
@@ -328,7 +334,7 @@ class PromptAcrSubscription extends AzureWizardPromptStep<WizardContext> {
          description: sub.subscription.subscriptionId || ''
       });
       const subPick = await wizardContext.ui.showQuickPick(
-         getAsyncOptions(subs, subToItem),
+         sort(getAsyncOptions(subs, subToItem)),
          {
             ignoreFocusOut,
             stepName: 'ACR Subscription',
@@ -369,7 +375,7 @@ class PromptAcrResourceGroup extends AzureWizardPromptStep<WizardContext> {
          label: rg.resourceGroup.name || ''
       });
       const rgPick = await wizardContext.ui.showQuickPick(
-         getAsyncOptions(rgs, rgToItem),
+         sort(getAsyncOptions(rgs, rgToItem)),
          {
             ignoreFocusOut,
             stepName: 'ACR Resource Group',
@@ -409,7 +415,7 @@ class PromptAcrRegistry extends AzureWizardPromptStep<WizardContext> {
          label: r.registry.name || ''
       });
       const registryPick = await wizardContext.ui.showQuickPick(
-         getAsyncOptions(registries, registryToItem),
+         sort(getAsyncOptions(registries, registryToItem)),
          {
             ignoreFocusOut,
             stepName: 'Registry',
@@ -451,7 +457,7 @@ class PromptAcrRepository extends AzureWizardPromptStep<WizardContext> {
          label: r.repositoryName
       });
       const repositoryPick = await wizardContext.ui.showQuickPick(
-         getAsyncOptions(repositories, repositoryToItem),
+         sort(getAsyncOptions(repositories, repositoryToItem)),
          {
             ignoreFocusOut,
             stepName: 'Repository',
@@ -495,7 +501,7 @@ class PromptAcrTag extends AzureWizardPromptStep<WizardContext> {
       );
       const tagToItem = (t: TagItem) => ({label: t.tag.name || ''});
       const tagPick = await wizardContext.ui.showQuickPick(
-         getAsyncOptions(tags, tagToItem),
+         sort(getAsyncOptions(tags, tagToItem)),
          {
             ignoreFocusOut,
             stepName: 'Tag',
