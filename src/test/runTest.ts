a78d1a8a6fa0d198c54fae7cef2f19c34381c5aa
David Gamero <david340804@gmail.com>
2023-01-06 17:28:23 -0500

    use 32bit windows vscode binary

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-
+import * as os from 'os';
 import {runTests} from '@vscode/test-electron';
 
 async function main() {
@@ -11,9 +11,11 @@ async function main() {
       // The path to test runner
       // Passed to --extensionTestsPath
       const extensionTestsPath = path.resolve(__dirname, './suite/index');
+      const platform =
+         os.platform() === 'win32' ? 'win32-x64-archive' : undefined;
 
       // Download VS Code, unzip it and run the integration test
-      await runTests({extensionDevelopmentPath, extensionTestsPath});
+      await runTests({extensionDevelopmentPath, extensionTestsPath, platform});
    } catch (err) {
       console.error('Failed to run tests');
       process.exit(1);
